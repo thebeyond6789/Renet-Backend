12e8c7d5a35033fcd007122fa2152cf9cd269a57
tonvanhoang <hoangton1210@gmail.com>
2024-10-24 14:56:11 +0700

    update models

--- models/post.js
+++ models/post.js
@@ -5,9 +5,18 @@ const post = new Schema({
   _id: {
     type: ObjectId,
   },
-  post: { type:Array},
+  post: [{ type: String }], // Mảng lưu tên file ảnh,
   title: { type: String },
-  datePost: { type: String },
+  datePost: { type: String ,
+    default:currentDate()
+  },
   idAccount: { type: String, ref: "account" },
 });
+function currentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${day}-${month}-${year}`;
+}
 module.exports = mongoose.models.post || mongoose.model("post", post);
